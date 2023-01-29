96eed34241978027ff1b7366e59bc8e15fc3d85d
Jammonjones <wjssaan@gmail.com>
2023-01-28 22:04:42 -0700

    mostly done

---
+++ chamber/scripts/script.js
@@ -0,0 +1,19 @@
+//get date and make it update on site header
+const today = new Date();
+const dayOfWeek = today.toLocaleDateString('en-US', { weekday: 'long' });
+const date = today.getUTCDate();
+const month = today.toLocaleDateString('en-US', { month: 'long' });
+const year = today.getUTCFullYear();
+const formattedDate = `${dayOfWeek}, ${date} ${month} ${year}`;
+const datee = document.querySelector("#date");
+datee.innerHTML = formattedDate;
+
+// Toggle menu open and closed
+const toggle = document.querySelector("#toggle");
+const menu = document.querySelector("#menu");
+
+function DoIt()
+{
+    menu.classList.toggle("open");
+}
+toggle.addEventListener("click", DoIt )
