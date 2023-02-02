fcb8c4ec91c1dfa6a8f6cd3f8ed49f413b9448b3
Jammonjones <wjssaan@gmail.com>
2023-02-02 15:32:17 -0700

    fixed 1 day behind error in date

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,7 @@
 //get date and make it update on site header
 const today = new Date();
 const dayOfWeek = today.toLocaleDateString('en-US', { weekday: 'long' });
-const date = today.getUTCDate() - 1;
+const date = today.getUTCDate();
 const month = today.toLocaleDateString('en-US', { month: 'long' });
 const year = today.getUTCFullYear();
 const formattedDate = `${dayOfWeek}, ${date} ${month} ${year}`;
