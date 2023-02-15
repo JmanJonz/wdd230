d8bfe1ca5383fd44e8ad57716700d6c3226db6fd
Jammonjones <wjssaan@gmail.com>
2023-02-14 18:18:39 -0700

    done

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,17 @@
+const temp = document.querySelector("#temp");
+const speed = document.querySelector("#speed");
+const chill = document.querySelector("#chill");
+
+const tempValue = parseInt(temp.innerHTML);
+const speedValue = parseInt(speed.innerHTML);
+const chillValue = parseInt(chill.innerHTML);
+
+let windchill = 35.74 + (0.6215 * tempValue) - (35.75 * speedValue ** 0.16) + (0.4275 * tempValue * speedValue ** 0.16);
+
+if (tempValue <= 50 && speedValue > 3)
+{
+    chill.innerHTML = Math.round(windchill);
+}else{
+    chill.innerHTML = "NA"
+}
+
