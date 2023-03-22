17e2f3693481576f9b0489ac4ddd53153ed998d5
Jammonjones <wjssaan@gmail.com>
2023-03-22 15:49:01 -0600

    Ready to be regraded

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,17 +1,23 @@
-const temp = document.querySelector("#temp");
-const speed = document.querySelector("#speed");
-const chill = document.querySelector("#chill");
+function WindChill(){
+    const temp = document.querySelector("#temp");
+    const speed = document.querySelector("#speed");
+    const chill = document.querySelector("#chill");
 
-const tempValue = parseInt(temp.innerHTML);
-const speedValue = parseInt(speed.innerHTML);
-const chillValue = parseInt(chill.innerHTML);
+    const tempValue = parseInt(temp.innerHTML);
+    console.log(tempValue);
+    const speedValue = parseInt(speed.innerHTML);
+    const chillValue = parseInt(chill.innerHTML);
 
-let windchill = 35.74 + (0.6215 * tempValue) - (35.75 * speedValue ** 0.16) + (0.4275 * tempValue * speedValue ** 0.16);
+    let windchill = 35.74 + (0.6215 * tempValue) - (35.75 * speedValue ** 0.16) + (0.4275 * tempValue * speedValue ** 0.16);
 
-if (tempValue <= 50 && speedValue > 3)
-{ 
-    chill.innerHTML = Math.round(windchill);
-}else{
-    chill.innerHTML = "NA"
+    if (tempValue <= 50 && speedValue > 3)
+    { 
+        chill.innerHTML = Math.round(windchill);
+    }else{
+        chill.innerHTML = "NA"
+    }
 }
+
+setTimeout(WindChill, 3000);
+
 
